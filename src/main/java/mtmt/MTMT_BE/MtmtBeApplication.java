8a38767f35ac822c2aa55cd7886e7b7ea662eca3
iamb0ttle <nbhyun0329@gmail.com>
2025-07-15 15:12:52 +0900

    chore : project setting

---
+++ src/main/java/mtmt/MTMT_BE/MtmtBeApplication.java
@@ -0,0 +1,13 @@
+package mtmt.MTMT_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtmtBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtmtBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mtmt/MTMT_BE/MtmtBeApplicationTests.java
@@ -0,0 +1,13 @@
+package mtmt.MTMT_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtmtBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
